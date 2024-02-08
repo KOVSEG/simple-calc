98af75ddf929d6452e1ea7b02af362aeefd643ce
Kovseg <vladislavkoritskiy@yandex.ru>
2024-02-08 23:42:06 +0300

    added js code for show numbers which linked to every button

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 let numsOnDisplay = document.querySelector('.numsondisplay');
 
-numsOnDisplay.textContent = '1234567890123456';
+let btnNumbers = document.querySelectorAll('.numbers .btn');
+
+let displayNumber = '';
+
+for(let num of btnNumbers) {
+  if(num.textContent === ',') {
+    num.addEventListener('click', function() {
+      num = num.textContent;
+      // showNumsOnDisplay(num);
+    });
+  } else {
+    num.addEventListener('click', function() {
+      console.log(num.textContent);
+      num = num.textContent;
+      // num = Number(num.textContent);
+      showNumsOnDisplay(num);
+    });
+  }
+};
+
+function showNumsOnDisplay(elems) {
+  displayNumber += elems;
+  // numsOnDisplay.textContent = displayNumber;
+};
+
+
+console.log(displayNumber);
