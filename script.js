335471c34a298d5ff0ce50a54d1b53d56c41f3ee
Kovseg <vladislavkoritskiy@yandex.ru>
2024-02-11 23:00:33 +0300

    got access for all buttons of calculator

--- script.js
+++ script.js
@@ -2,29 +2,6 @@ let numsOnDisplay = document.querySelector('.numsondisplay');
 
 let btnNumbers = document.querySelectorAll('.numbers .btn');
 
-let getNums = '';
-let displayNumber = [];
-let history = [];
+let btnCleaners = document.querySelectorAll('.fix .btn');
 
-for(let num of btnNumbers) {
-  if(num.value === ',') {
-    num.addEventListener('click', func);
-    function func() {
-      getNums += this.value;
-      num.removeEventListener('click', func);
-      showNumsOnDisplay();
-    };
-  } else {
-    num.addEventListener('click', function() {
-      getNums += this.value;
-      showNumsOnDisplay();
-    });
-  }
-};
-
-function showNumsOnDisplay() {
-  if(getNums.length <= 16) {
-    numsOnDisplay.textContent = getNums;
-    displayNumber.push(getNums);
-  };
-};
+let btnOperators = document.querySelectorAll('.operators .btn');
