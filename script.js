90c5d728b24b62e978f02666e757c56adee4c3e2
Kovseg <vladislavkoritskiy@yandex.ru>
2024-02-12 13:48:17 +0300

    deleted number 0 in html, added code for all buttons and code to turn on calculator

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 let numsOnDisplay = document.querySelector('.numsondisplay');
+let calculator = document.querySelector('.calculator');
+let btnCommon = document.querySelectorAll('.btn');
+let getNums = '';
 
-let btnNumbers = document.querySelectorAll('.numbers .btn');
+// Turn on the calculator
+calculator.addEventListener('mouseover', startCalc);
+function startCalc() {
+  numsOnDisplay.textContent = '0'; //Show number 0 when calc is on
+  calculator.removeEventListener('mouseover', startCalc);
+};
 
-let btnCleaners = document.querySelectorAll('.fix .btn');
-
-let btnOperators = document.querySelectorAll('.operators .btn');
+// Get value of every button
+for(let button of btnCommon) {
+  button.addEventListener('click', function func() {
+    if(this.classList.contains('number')) {
+      getNums += this.value;
+      numsOnDisplay.textContent = getNums; //Show nums on display
+    };
+    if(this.value === ',') {
+      this.removeEventListener('click', func); //Restrict commas of number
+    };
+  });
+};
