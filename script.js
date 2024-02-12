83e2b4105c1034659c6d8100d24b53a06afd8973
Kovseg <vladislavkoritskiy@yandex.ru>
2024-02-12 22:12:38 +0300

    added a function which check number 0 before dot

--- script.js
+++ script.js
@@ -13,12 +13,25 @@ function startCalc() {
 // Get value of every button
 for(let button of btnCommon) {
   button.addEventListener('click', function func() {
+    if(this.value === ',') {
+      writeZeroBeforeDot();
+      this.removeEventListener('click', func); //Restrict commas of number
+    };
     if(this.classList.contains('number')) {
       getNums += this.value;
       numsOnDisplay.textContent = getNums; //Show nums on display
     };
-    if(this.value === ',') {
-      this.removeEventListener('click', func); //Restrict commas of number
-    };
+
   });
 };
+
+// Write zero first if the first call is a decimal point
+function writeZeroBeforeDot() {
+  if(getNums == '') {
+    getNums = '0';
+  };
+};
+
+
+// Limit the number of digits on display
+// Forbid to repeat a value 00 before dot, after dot it can repeat many times
