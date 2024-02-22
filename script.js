8a038acae652ac3c10220397ed9d73b6bf5c79f4
Kovseg <vladislavkoritskiy@yandex.ru>
2024-02-22 13:02:25 +0300

    changed js code for calc, added to html additional classes

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
 let numsOnDisplay = document.querySelector('.numsondisplay');
+let calculator = document.querySelector('.calculator');
+let btnCommon = document.querySelectorAll('.btn');
+let getBtnValue = '';
+let summary = '';
 
-let btnNumbers = document.querySelectorAll('.numbers .btn');
-
-let getNums = '';
-let displayNumber = [];
-let history = [];
-
-for(let num of btnNumbers) {
-  if(num.value === ',') {
-    num.addEventListener('click', func);
-    function func() {
-      getNums += this.value;
-      num.removeEventListener('click', func);
-      showNumsOnDisplay();
-    };
-  } else {
-    num.addEventListener('click', function() {
-      getNums += this.value;
-      showNumsOnDisplay();
-    });
-  }
+// Turn on the calculator
+calculator.addEventListener('mouseover', startCalc);
+function startCalc() {
+  numsOnDisplay.textContent = '0'; //Show number 0 when calc is on
+  calculator.removeEventListener('mouseover', startCalc);
 };
 
-function showNumsOnDisplay() {
-  if(getNums.length <= 16) {
-    numsOnDisplay.textContent = getNums;
-    displayNumber.push(getNums);
+// Get value of every button
+for(let button of btnCommon) {
+  button.addEventListener('click', func);
+};
+
+function func() {
+
+  if(this.classList.contains('number')) {
+    getBtnValue = this.value;
+
+    if(this.value == ',') {
+      this.removeEventListener('click', func);
+    } else {
+      getBtnValue = this.value;
+    };
+
   };
+
+  if(this.classList.contains('fix')) {
+    console.log(this);
+  };
+
+  if(this.classList.contains('operate')) {
+    console.log(this);
+  };
+
+  sumFunc(getBtnValue);
+};
+
+
+function sumFunc(getNumber) {
+
+  //Nums less than 16 digits
+  if(getNumber.length <= 16) {
+    numsOnDisplay.textContent = getNumber;
+  };
+
 };
